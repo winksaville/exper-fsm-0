50e186d580614bb6d8c99570d0418f78bbd1940f
Wink Saville <wink@saville.com>
2022-05-09 13:25:31 -0700

    refactor: Move process_msg into StateMachine
    Doing this the state machine works! BUT, process_msg is going to get
    complicated. I'm guessing macros will be needed to make this feasible.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,34 @@
-// A trait that processes a message and drives the state machine
-trait ProcessMsg {
-    fn process_msg(&self, sm: &mut StateMachine, msg: &Protocol1);
-}
-
 // Create a Protocal with one message
 enum Protocol1 {
     Msg1 { f1: i32 },
 }
 
+enum States {
+    State1,
+}
+
 struct StateMachine {
-    current_state: Box<dyn ProcessMsg>,
+    current_state: States,
     data1: i32,
 }
 
 impl StateMachine {
-    fn new(initial_state: Box<dyn ProcessMsg>) -> Self {
+    fn new(initial_state: States) -> Self {
         StateMachine {
             current_state: initial_state,
             data1: 0,
         }
     }
 
-    fn dispatch(&mut self, msg: &Protocol1) {
-        self.current_state.process_msg(self, msg);
-    }
-}
-
-struct State1;
-
-impl ProcessMsg for State1 {
-    fn process_msg(&self, sm: &mut StateMachine, msg: &Protocol1) {
-        match *msg {
-            Protocol1::Msg1 { f1 } => {
-                sm.data1 += 1;
-                println!("State1: process sm.data1={} Msg1::f1={}", sm.data1, f1);
+    fn process_msg(&mut self, msg: &Protocol1) {
+        match self.current_state {
+            States::State1 => {
+                match *msg {
+                    Protocol1::Msg1 { f1 } => {
+                        self.data1 += 1;
+                        println!("State1: process sm.data1={} Msg1::f1={}", self.data1, f1);
+                    }
+                }
             }
         }
     }
@@ -41,10 +36,10 @@ impl ProcessMsg for State1 {
 
 fn main() {
     // Create state machine with its initial state
-    let initial_state = Box::new(State1);
+    let initial_state = States::State1;
     let mut sm = StateMachine::new(initial_state);
 
     // Create a message and dispatch it to the state machine
     let msg = Protocol1::Msg1 { f1: 123 };
-    sm.dispatch(&msg);
+    sm.process_msg(&msg);
 }
